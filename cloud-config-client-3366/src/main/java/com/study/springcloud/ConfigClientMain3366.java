32bbebb8d31e0a7855fbf2528c3327d8d8d0b6b5
amadman <466456543@qq.com>
2020-11-22 09:26:56 +0800

    2020-11-22

---
+++ cloud-config-client-3366/src/main/java/com/study/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,17 @@
+package com.study.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author wang
+ * @create 2020-11-22-9:24
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366 {
+    public static void main(String[] args) {
+        SpringApplication.run( ConfigClientMain3366.class,args);
+    }
+}
