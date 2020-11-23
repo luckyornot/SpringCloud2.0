71646d53dcd2e91be3768c48b6ef8b49fdbbd131
amadman <466456543@qq.com>
2020-11-23 10:45:02 +0800

    2020-11-22 nacos集群完毕

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/study/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,23 @@
+package com.study.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wang
+ * @create 2020-11-22-18:57
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController
+{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo() {
+        return configInfo;
+    }
+}
